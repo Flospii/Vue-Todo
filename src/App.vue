4d517c7cdaa44231cae7951ea83c996fe0b18f38
Flospi <spitzbart.flo@hotmail.com>
2023-05-18 13:47:53 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="bg-gray-600 text-white">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">Unsere Todos</h1>
+  </div>
+  <div v-for="(todo, index) in todos" :key="todo.todo">
+    <ToDoComponent 
+    :todoprop="todo" 
+    :todoindex="index" 
+    :creationTimestamp="creationTimestamp" 
+    @toggledone-index="toggleDone(index)" 
+    @deleteTodo-index="deleteTodo(index)"/>
+  </div>
+  <div class="flex justify-between items-center py-2 px-2">
+    <input type="text" v-model="myText" class="border-2 border-gray-400 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500 text-black"/>
+    <button @click="addTodo(myText)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Einfügen</button>
+  </div>
+</div>  
+</template>
+
+<script>
+import ToDoComponent from './components/ToDoComponent.vue'
+import { v4 as uuidv4 } from 'uuid';
+export default {
+  name: 'App',
+  data(){
+    return{
+      todos:[],
+    };
+  },
+  methods:{
+    toggleDone(index){
+      this.todos[index].done = !this.todos[index].done;
+      this.updateData(this.todos.at(index));
+    },
+    deleteTodo(index){
+      console.log("App: " + index);
+      this.deleteData(this.todos.at(index));
+      this.todos.splice(index,1);
+    },
+    
+    addTodo(name){
+      if(name == null || name.trim() === ""){
+        return;
+      }
+      const todo = {_id: uuidv4(), creationTimestamp: new Date(), todo:name, done:false}
+      console.log(this.i);
+      this.todos.push(todo);
+      this.saveData(todo);
+    },
+    saveDataFromLocalStorage(){
+      localStorage.setItem("todos", JSON.stringify(this.todos))
+    },
+    fetchDataFromLocalStorage(){
+      let data = localStorage.getItem("todos");
+      if(data!=="" && data != null){
+      this.todos = JSON.parse(data);
+      }else{
+      this.todo = [];
+      }
+    },
+    //Talk to API
+    fetchData(){
+      fetch('api/todos')
+        .then(response => response.json())
+        .then(data => {
+          this.todos = data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    deleteData(todo){
+      fetch('api/todos/'+ todo._id, {
+        method: 'DELETE',
+        
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    updateData(todo){
+      fetch('api/todos/'+ todo._id, {
+        method: 'PUT',
+        body: JSON.stringify(todo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    saveData(todo){
+      fetch('api/todos', {
+        method: 'POST',
+        body: JSON.stringify(todo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+    },
+  mounted(){ //Called before loading 
+    this.fetchData();
+  },
+  components: {
+    ToDoComponent
+  }
+}
+</script>
+
---
+++ src/components/ToDoComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div @click="toggleTodo()" class="cursor-pointer flex justify-between items-center p-2 m-2"
+   :class="{'bg-green-500': this.todo.done, 'bg-red-500': !this.todo.done}" >
+    <h1 >{{todo.todo}}</h1>
+    <p>{{ formatDate(todo.creationTimestamp) }}</p>
+   <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" @click="deleteTodo()">Löschen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoComponent',
+  props: ["todoprop", "todoindex", "creationTimestamp"],
+  data() {
+    return {
+      todo: this.todoprop,
+    };
+  },
+  methods:{
+    toggleTodo(){
+      this.$emit("toggledone-index", this.todoindex);
+    },
+    deleteTodo(){
+      this.$emit("deleteTodo-index", this.todoindex);
+    },
+    formatDate(timestamp) {
+      const date = new Date(timestamp);
+      const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
+      return date.toLocaleString('en-US', options);
+    },
+  },
+};
+</script>
+
